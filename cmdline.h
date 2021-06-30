0a7b52deb91b9e7148b94bde4e1031e0919ef75c
theseed <feiyuxin1000@sina.com>
2021-06-30 09:42:43 +0800

    use C++11 to simplify code

--- cmdline.h
+++ cmdline.h
@@ -38,6 +38,7 @@
 #include <algorithm>
 #include <cxxabi.h>
 #include <cstdlib>
+#include <type_traits>
 
 namespace cmdline{
 
@@ -86,20 +87,10 @@ public:
   }
 };
 
-template <typename T1, typename T2>
-struct is_same {
-  static const bool value = false;
-};
-
-template <typename T>
-struct is_same<T, T>{
-  static const bool value = true;
-};
-
 template<typename Target, typename Source>
 Target lexical_cast(const Source &arg)
 {
-  return lexical_cast_t<Target, Source, detail::is_same<Target, Source>::value>::cast(arg);
+  return lexical_cast_t<Target, Source, std::is_same<Target, Source>::value>::cast(arg);
 }
 
 static inline std::string demangle(const std::string &name)
@@ -169,6 +160,8 @@ range_reader<T> range(const T &low, const T &high)
 
 template <class T>
 struct oneof_reader{
+  oneof_reader(std::initializer_list<T> l) : alt(l) {}
+
   T operator()(const std::string &s){
     T ret=default_reader<T>()(s);
     if (std::find(alt.begin(), alt.end(), ret)==alt.end())
@@ -181,128 +174,9 @@ private:
 };
 
 template <class T>
-oneof_reader<T> oneof(T a1)
+oneof_reader<T> oneof(std::initializer_list<T> l)
 {
-  oneof_reader<T> ret;
-  ret.add(a1);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  ret.add(a8);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8, T a9)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  ret.add(a8);
-  ret.add(a9);
-  return ret;
-}
-
-template <class T>
-oneof_reader<T> oneof(T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8, T a9, T a10)
-{
-  oneof_reader<T> ret;
-  ret.add(a1);
-  ret.add(a2);
-  ret.add(a3);
-  ret.add(a4);
-  ret.add(a5);
-  ret.add(a6);
-  ret.add(a7);
-  ret.add(a8);
-  ret.add(a9);
-  ret.add(a10);
-  return ret;
+  return oneof_reader<T>(l);
 }
 
 //-----

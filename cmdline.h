01b692457df7e74c9b4d9a0b3fae37451e86bfb0
theseed <feiyuxin1000@sina.com>
2021-07-23 16:28:39 +0800

    Update cmdline.h

--- cmdline.h
+++ cmdline.h
@@ -246,10 +246,6 @@ public:
   }
 
   std::string error() const{
-    return errors.size()>0?errors[0]:"";
-  }
-
-  std::string error_full() const{
     std::ostringstream oss;
     for (size_t i=0; i<errors.size(); i++)
       oss<<errors[i]<<std::endl;
